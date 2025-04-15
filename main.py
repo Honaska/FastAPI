4dc449e8e7e41f718174d8dd1cd757db733ce867
Tantoy, James Ryan <100672289+Honaska@users.noreply.github.com>
2025-04-15 23:11:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List
+from typing import Optional
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -18,7 +18,7 @@ class TaskModel(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    details = Column(String, nullable=True)
+     details: Optional[str] = None
 
 Base.metadata.create_all(bind=engine)
 
